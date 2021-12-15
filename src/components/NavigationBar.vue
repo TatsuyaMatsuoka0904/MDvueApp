990c470266dd4f4b7d333cf375fb7b5d52ad2ab9
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2021-12-15 18:25:43 +0900

    Created study.vue vue router reflection App SPA study events add and text javascript created

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
-    <a class="navbar-brand" href="/">TechpitNotion</a>
+    <a class="navbar-brand" href="/main">TechpitNotion</a>
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -11,7 +11,7 @@
       <ul class="navbar-nav">
 
         <li class="nav-item">
-          <a class="nav-link" href="#"><i class="fas fa-file-alt"></i> ノートリスト</a>
+          <a class="nav-link" href="/main"><i class="fas fa-file-alt"></i> ノートリスト</a>
         </li>
 
         <li class="nav-item dropdown">
@@ -19,9 +19,9 @@
             <i class="fas fa-user-graduate"></i> 学習用ページ
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">学習1</a></li>
-            <li><a class="dropdown-item" href="#">学習2</a></li>
-            <li><a class="dropdown-item" href="#">学習3</a></li>
+            <li><a class="dropdown-item" href="/study/binding">学習1</a></li>
+            <li><a class="dropdown-item" href="/study/directive">学習2</a></li>
+            <li><a class="dropdown-item" href="/study/event">学習3</a></li>
           </ul>
         </li>
       </ul>
---
+++ src/components/study/BindingPage.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="binding-page">
+        <h2>学習１:双方向バインディング</h2>
+        
+        <h3><span class="badge badge-secondary">Text</span></h3>
+        <input class="form-control" id="name" />
+        <span id="text"></span>
+        <br>
+        <br>
+        
+        <h3><span class="badge badge-secondary">Vue.js</span></h3>
+        <input class='form-control' v-model="name">
+        <span>{{ name }}</span>
+    </div>
+</template>
+
+<script>
+window.onload = function load () {
+    const nameInput = document.getElementById('name');
+    nameInput.oninput = function() {
+        const name = nameInput.value;
+        const text = document.getElementById('text');
+        text.innerHTML = '入力された名前は' +  name + 'です';
+    };
+}
+
+export default {
+    data() {
+        return{
+            name : '',
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+.binding-page {
+    padding: 20px;
+}
+
+</style>
